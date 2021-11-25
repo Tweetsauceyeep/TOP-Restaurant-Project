d84f6510efbff630b6549d5ff7456cee1c6d68fb
Tweetsauce <pobusauce12@gmail.com>
2021-11-25 20:11:16 +0400

    added menu and contact pages. Work on page switching logic next

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import createHome from './pageLoad.js' 
 import contactPage from './contact.js'
-
+import menuPage from './menu.js'
 console.log('check if worky')
 
 const content = document.querySelector('#content')
-
-
-
-content.appendChild(contactPage())
+content.appendChild(menuPage())
 
 
---
+++ src/menu.js
@@ -0,0 +1,19 @@
+function menuPage(){
+  const menu = document.createElement('div');
+ 
+  menu.appendChild(createParagraph('Chicken Adobo: $14'))
+  menu.appendChild(createParagraph('Chicken Nilaga: $15'))
+  menu.appendChild(createParagraph('Pork Adobo: $16'))
+  menu.appendChild(createParagraph('Fresh Fruit: $21'))
+
+  return menu
+}
+
+function createParagraph(text){
+  const paragraph = document.createElement('p')
+  paragraph.textContent = text
+
+  return paragraph
+}
+
+export default menuPage
