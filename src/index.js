4acaa5f180c61496960973e139eeaec1075236f5
Tweetsauce <pobusauce12@gmail.com>
2021-11-25 23:03:27 +0400

    added navbar

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
 import createHome from './pageLoad.js' 
 import contactPage from './contact.js'
 import menuPage from './menu.js'
-console.log('check if worky')
 
+console.log('check if worky')
 const content = document.querySelector('#content')
-content.appendChild(menuPage())
+
+// Create Nav bar in index.js file so that it remains in all pages
+function navBar(){
+  const navbar = document.createElement('nav')
+   
+  navbar.appendChild(navButton('Home'))
+  navbar.appendChild(navButton('Contact'))  
+  navbar.appendChild(navButton('Menu'))
+  return navbar;
+}
+
+function navButton(idName){
+  const button = document.createElement('button')
+  button.id = idName.toLowerCase(); // id is lowercase
+  button.textContent = idName;
+  return button;
+}
+
+
+//ayy?
+content.appendChild(navBar());
+content.appendChild(menuPage());
 
 
